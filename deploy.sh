978890bb96a5ad8eaad85dcbd818d0e9791fbde9
fcabanasm <franco.cabanas@ceinf.cl>
2020-03-13 13:30:14 -0300

    deploy fix

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker build -t fcabanasm/kubernetes-client:latest -t fcabanasm/kubernetes-client:$SHA -f ./client/Dockerfile ./client
-docker build -t fcabanasm/kubernetes-server -t fcabanasm/kubernetes-server:$SHA -f ./server/Dockerfile ./server
+docker build -t fcabanasm/kubernetes-server:latest -t fcabanasm/kubernetes-server:$SHA -f ./server/Dockerfile ./server
 
 docker push fcabanasm/kubernetes-client:latest
 docker push fcabanasm/kubernetes-server:latest
@@ -8,5 +8,5 @@ docker push fcabanasm/kubernetes-client:$SHA
 docker push fcabanasm/kubernetes-server:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments-server-deployment server=fcabanasm/kubernetes-server:$SHA
-kubectl set image deployments-client-deployment client=fcabanasm/kubernetes-client:$SHA
+kubectl set image deployments/server-deployment server=fcabanasm/kubernetes-server:$SHA
+kubectl set image deployments/client-deployment client=fcabanasm/kubernetes-client:$SHA
